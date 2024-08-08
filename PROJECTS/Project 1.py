243122754e495648ca4a41e5d2598723bff0cfb4
Deepak <d03748651@gmail.com>
2024-08-08 12:27:07 +0530

    Add files via upload

---
+++ PROJECTS/Project 1.py
@@ -0,0 +1,44 @@
+# this is Gonna be a game called madlib in which we blank random word in phrases and than you choose the 
+#
+
+def madlib1():
+    Name1 = input("name1:")
+    Name2 = input("name 2:")
+    Adjective1 = input("adj1:")
+    Animal1 = input("Animal1:")
+    Emotion1 = input("emotion")
+    verb1 = input("verb1:")
+    adjective2 = input("adj2:")
+    clothing_item = input("clothingitem:")
+
+    madlib_story = f"Ek din {Name1} aur {Name2} jungle mein ghoomne gaye.
+    Wahan unhone ek {Adjective1} {Animal1} dekha. Ve bahut {Emotion1} hue aur {verb1} karne lage. Achanak,
+    {adjective2} {Animal1} ne {clothing_item} pehan liya ""
+    print(madlib_story)
+
+
+def madlib2():
+    Animal = input("animal:")
+    verb = input("verb:")
+    place = input("place:")
+    adjective = input("adj")
+    object = input("object:")
+    emotion = input("emotion:")
+    madlib_story = f"Ek {Animal} ne {verb} kiya aur {place} gaya. 
+    Wahan usne ek {adjective} {object} dekha aur {emotion} ho gaya."
+    print(madlib_story)
+
+def madlib3():
+    Animal = input("animal:")
+    Adjective = input("adj:")
+    Object = input("object:")
+    Place = input("place:")
+    Verb = input("verb:")
+    Emotion = input("Emotion:")
+
+    madlib_story = f"{Animal} ek {Adjective} {Object} le kar {Place} gaya. Wahan usne
+    {Verb} kiya aur {Emotion} feel kiya."
+
+def madlb3():
+    
+
